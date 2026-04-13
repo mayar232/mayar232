afc7642a52d345d31bee463adb203275c7f11eab
mayar <miyaralkhalili@gmail.com>
2026-04-13 23:27:16 +0400

    Update واجب.py

--- واجب.py
+++ واجب.py
@@ -7,4 +7,14 @@ for i in range(1):
     for j in range(1,len(listt)):
         add.append(listt[j])
     print(add)
-    
+    #######################
+listt = ["Ali","Muna","Ahmed","Reem","Nuha"]
+add = []
+
+add.append("mayar")
+
+for i in listt:
+    if i not in add:
+        add.append(i)
+
+print(add)
